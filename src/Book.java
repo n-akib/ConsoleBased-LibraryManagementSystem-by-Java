0b907f339157a3b026e5ff51b41ae7bacc25b792
n-akib <n.mahfuze@gmail.com>
2025-04-24 12:25:11 +0600

    Design the Book class

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,50 @@
 public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private String category;
+    private int totalCopies;
+    private int availableCopies;
+
+
+    public Book(int id, String title, String author, String category, int totalCopies){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.totalCopies =totalCopies;
+        this.availableCopies = totalCopies;
+
+    }
+    public int getId() {
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public int getTotalCopies(){
+        return totalCopies;
+    }
+    public int getAvailableCopies(){
+        return availableCopies;
+    }
+    public void setAvailableCopies(int count){
+        this.availableCopies = count;
+    }
+
+    public String toString(){
+        return "Book ID:" + id + ", Title: " + title + ", Author: " + author + ", Category: " + ", Available Copies: "
+                + availableCopies + "/" + totalCopies;
+    }
+
+
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("--Library Menu--");
+            System.out.println(" 1. Add Book");
+            System.out.println(" 2. View All Books");
+            System.out.println(" 0. Exit");
+            System.out.println("Choose an Option: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
+        }while (choice != 0);
 
+        scanner.close();
     }
 }
 
